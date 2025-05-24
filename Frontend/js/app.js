1705f516d4cfbfcbf0225c1bbe2e7ff2853778ae
ameshanethmini <ameshanethmini25@gmail.com>
2025-05-24 07:06:13 +0530

    wrong backend delete

--- Frontend/js/app.js
+++
@@ -1,8 +0,0 @@
-console.log('Hello from app.js');
-
-function showSection(id) {
-    
-    const sections = document.querySelectorAll('.section');
-    sections.forEach(section => section.classList.remove('active'));
-    document.getElementById(id).classList.add('active');
-  }
